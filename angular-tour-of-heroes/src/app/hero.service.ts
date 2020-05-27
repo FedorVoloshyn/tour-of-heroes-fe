c3e7d84c31ebd11a50874f50aee158617617c059
fedor.voloshyn <fedor.voloshyn@gmail.com>
2020-05-27 17:43:15 +0300

    configured development mode

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -6,21 +6,51 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'http://localhost:1488/api/heroes';
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient,
+              private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    const url = `${this.heroesUrl}`;
+    return this.http.get<Hero[]>(url).pipe(
+      tap(_ => this.messageService.add('HeroService: fetched heroes')),
+      catchError(this.handleError<Hero[]>(`getHeroes`))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.messageService.add(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
+
+  pushHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.messageService.add(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
 }
